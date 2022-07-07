41502f4cd6a337c3eb53022868a66c2cc86979b5
Akshat <akshatgauriiitsurat@gmail.com>
2022-07-07 22:34:58 +0530

    final

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,10 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+      <ChakraProvider resetCSS theme={theme}>
+    <Head>
+      <title>Flask_app</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
